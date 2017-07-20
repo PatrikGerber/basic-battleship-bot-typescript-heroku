33b94228d6e9ef1cc15ae2de127ecd04c891feeb
Patrik Gerber <Patrik.Gerber@softwire.com>
2017-07-20 17:13:35 +0100

    Nothing is working

--- src/position.ts
+++ src/position.ts
@@ -4,7 +4,7 @@ export class Position{
     public row:number;
     public column:number;
     constructor(pos:{"Row":string, "Column":number}){
-        this.row = GameState.backConverter[pos.Row];
+        this.row = GameState.converter[pos.Row];
         this.column = pos.Column - 1;
     }
     public structure():{"Row":string, "Column":number}{
