7bbdb8d97df00b3ebe8a58f6a90007327a867bb4
Patrik Gerber <Patrik.Gerber@softwire.com>
2017-07-20 16:55:00 +0100

    Trying to make things work

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -31,39 +31,39 @@ export class MyBot {
         return gamestate.randomDraw().json();
     }
 
-    public huntNextTarget(gamestate:GameState, huntHitCount:number):{"Row":string, "Column":number}{
-        if (huntHitCount == 1){
-            let hitPosition:{"Row":string, "Column":number};
-            let pos:number;
-            for (let i:number = 1; i <= Math.min(gamestate.MyShots.length,4); i++){
-                if (gamestate.MyShots[gamestate.MyShots.length-i].WasHit){
-                    pos = i;
-                    break;
-                }
-            }
+    // public huntNextTarget(gamestate:GameState, huntHitCount:number):{"Row":string, "Column":number}{
+    //     if (huntHitCount == 1){
+    //         let hitPosition:{"Row":string, "Column":number};
+    //         let pos:number;
+    //         for (let i:number = 1; i <= Math.min(gamestate.MyShots.length,4); i++){
+    //             if (gamestate.MyShots[gamestate.MyShots.length-i].WasHit){
+    //                 pos = i;
+    //                 break;
+    //             }
+    //         }
 
-            hitPosition = gamestate.MyShots[gamestate.MyShots.length-pos];
-            let row:number = GameState.converter[hitPosition.Row]; // 0 indexed
-            let column:number = hitPosition.Column-1; // 0 indexed
+    //         hitPosition = gamestate.MyShots[gamestate.MyShots.length-pos];
+    //         let row:number = GameState.converter[hitPosition.Row]; // 0 indexed
+    //         let column:number = hitPosition.Column-1; // 0 indexed
 
-            if (gamestate.isValidTarget(row, column+1)) {
-                console.log("Actually returning something")
-                return {"Row":GameState.backConverter[row], "Column":column+2};
-            }
-            if (gamestate.isValidTarget(row, column-1)) {
-                console.log("Actually returning something")
-                return {"Row":GameState.backConverter[row], "Column":column};
-            }
-            if (gamestate.isValidTarget(row+1, column)) {
-                console.log("Actually returning something")
-                return {"Row":GameState.backConverter[row+1], "Column":column+1};
-            }
-            if (gamestate.isValidTarget(row-1, column)) {
-                console.log("Actually returning something")
-                return {"Row":GameState.backConverter[row-1], "Column":column+1};
-            }
-        }
-        return this.getRandomNextTarget(gamestate);
-    }
+    //         if (gamestate.isValidTarget(row, column+1)) {
+    //             console.log("Actually returning something")
+    //             return {"Row":GameState.backConverter[row], "Column":column+2};
+    //         }
+    //         if (gamestate.isValidTarget(row, column-1)) {
+    //             console.log("Actually returning something")
+    //             return {"Row":GameState.backConverter[row], "Column":column};
+    //         }
+    //         if (gamestate.isValidTarget(row+1, column)) {
+    //             console.log("Actually returning something")
+    //             return {"Row":GameState.backConverter[row+1], "Column":column+1};
+    //         }
+    //         if (gamestate.isValidTarget(row-1, column)) {
+    //             console.log("Actually returning something")
+    //             return {"Row":GameState.backConverter[row-1], "Column":column+1};
+    //         }
+    //     }
+    //     return this.getRandomNextTarget(gamestate);
+    // }
 }
 
